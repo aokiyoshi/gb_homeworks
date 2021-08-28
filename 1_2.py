b0910e0f37fcaeabf1a6b848f39f11d60a95fe61
Ilshat Kulbaev <87938357+aokiyoshi@users.noreply.github.com>
2021-08-28 23:58:56 +0500

    Update 1_2.py
    1_2 исправлен

--- 1_2.py
+++ 1_2.py
@@ -1,4 +1,4 @@
-def sum_of_digits2(num):
+def sum_of_digits(num):
     summ = 0
     while num:
         digit = num % 10
@@ -6,24 +6,25 @@ def sum_of_digits2(num):
         summ += digit
     return summ
 
-cubes = [x*x*x for x in range(1, 1000)]
+cubes = [x*x*x for x in range(1, 1000) if x % 2 != 0]
 summ = 0
 for i in cubes:
-    if sum_of_digits2(i) % 7 == 0:
+    if sum_of_digits(i) % 7 == 0:
         summ += i 
 
 print(summ)
 
-cubes = [x ** 3 + 17 for x in range(1, 1000)]
+cubes = [x ** 3 + 17 for x in range(1, 1000) if x % 2 != 0]
+summ = 0
 summ = 0
 for i in cubes:
-    if sum_of_digits2(i) % 7 == 0:
+    if sum_of_digits(i) % 7 == 0:
         summ += i 
 
 print(summ)
 
-print(sum([num for num in [n ** 3 for n in range(1, 1000)]
+print(sum([num for num in [n ** 3 for n in range(1, 1000)  if n % 2 != 0]
           if sum([int(i) for i in list(str(num))]) % 7 == 0]))
 
-print(sum([num for num in [n ** 3 + 17 for n in range(1, 1000)]
+print(sum([num for num in [n ** 3 + 17 for n in range(1, 1000) if n % 2 != 0]
           if sum([int(i) for i in list(str(num))]) % 7 == 0]))
