b8f8bcf145298d3c02c8cc7c1e2767bf1a1cc4d9
Ilshat Kulbaev <87938357+aokiyoshi@users.noreply.github.com>
2021-08-29 10:55:04 +0500

    убраны тесты
    убраны тесты (не нужны в рамках задачи)

--- 1_3.py
+++ 1_3.py
@@ -1,6 +1,3 @@
-import unittest
-
-
 def format_procent(num):
     if num % 10 == 1:
         return f"{num} процент"
@@ -12,31 +9,8 @@ def format_procent(num):
         return f"{num} процентов"
 
 
-class TestFormatFunc(unittest.TestCase):
-    def test1(self):
-        self.assertEqual(format_procent(1), "1 процент")
-
-    def test2(self):
-        self.assertEqual(format_procent(2), "2 процента")
-
-    def test3(self):
-        self.assertEqual(format_procent(54), "54 процента")
-
-    def test4(self):
-        self.assertEqual(format_procent(13), "13 процентов")
-
-    def test5(self):
-        self.assertEqual(format_procent(5), "5 процентов")
-
-    def test5(self):
-        self.assertEqual(format_procent(5), "5 процентов")
-
-    def test6(self):
-        self.assertEqual(format_procent(99), "99 процентов")
-
-    def test7(self):
-        self.assertEqual(format_procent(101), "101 процент")
-
-
-if __name__ == "__main__":
-    unittest.main()
+print(format_procent(1))
+print(format_procent(25))
+print(format_procent(93))
+print(format_procent(41))
+print(format_procent(10))
